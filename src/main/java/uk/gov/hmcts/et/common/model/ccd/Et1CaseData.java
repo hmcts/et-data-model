babb92054d179fba3d9a9354efe6f1195214cfb5
Marc Judge <marc.judge@HMCTS.NET>
2022-05-04 16:17:32 +0100

    Add Claimant Request Type

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/Et1CaseData.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/Et1CaseData.java
@@ -3,15 +3,9 @@ package uk.gov.hmcts.et.common.model.ccd;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import uk.gov.hmcts.et.common.model.ccd.types.AdditionalCaseInfoType;
+import uk.gov.hmcts.et.common.model.ccd.types.*;
 import uk.gov.hmcts.et.common.model.ccd.items.JurCodesTypeItem;
-import uk.gov.hmcts.et.common.model.ccd.types.ClaimantIndType;
-import uk.gov.hmcts.et.common.model.ccd.types.ClaimantType;
 import uk.gov.hmcts.et.common.model.ccd.items.RespondentSumTypeItem;
-import uk.gov.hmcts.et.common.model.ccd.types.ClaimantWorkAddressType;
-import uk.gov.hmcts.et.common.model.ccd.types.ClaimantOtherType;
-import uk.gov.hmcts.et.common.model.ccd.types.RepresentedTypeC;
-import uk.gov.hmcts.et.common.model.ccd.types.NewEmploymentType;
 
 import java.util.List;
 
@@ -46,4 +40,6 @@ public class Et1CaseData {
     private String managingOffice;
     @JsonProperty("newEmploymentType")
     private NewEmploymentType newEmploymentType;
+    @JsonProperty("claimantRequests")
+    private ClaimantRequestType claimantRequests;
 }
