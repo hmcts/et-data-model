7da68f686c08dafce7bd43705907697450bc2088
lvo700 <lynnette.orr@HMCTS.net>
2022-05-04 10:57:24 +0100

    Added claimant other type fields

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/types/ClaimantOtherType.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/types/ClaimantOtherType.java
@@ -20,6 +20,26 @@ public class ClaimantOtherType {
     private String claimantEmployedNoticePeriod;
     @JsonProperty("claimant_disabled")
     private String claimantDisabled;
-    @JsonProperty("claimant_disabled_details")
-    private String claimantDisabledDetails;
+    @JsonProperty("claimant_notice_period")
+    private String claimantNoticePeriod;
+    @JsonProperty("claimant_notice_period_unit")
+    private String claimantNoticePeriodUnit;
+    @JsonProperty("claimant_notice_period_duration")
+    private String claimantNoticePeriodDuration;
+    @JsonProperty("claimant_average_weekly_hours")
+    private String claimantAverageWeeklyHours;
+    @JsonProperty("claimant_pay_before_tax")
+    private String claimantPayBeforeTax;
+    @JsonProperty("claimant_pay_after_tax")
+    private String claimantPayAfterTax;
+    @JsonProperty("claimant_pay_cycle")
+    private String claimantPayCycle;
+    @JsonProperty("claimant_pension_contribution")
+    private String claimantPensionContribution;
+    @JsonProperty("claimant_pension_weekly_contribution")
+    private String claimantPensionWeeklyContribution;
+    @JsonProperty("claimant_benefits")
+    private String claimantBenefits;
+    @JsonProperty("claimant_benefits_detail")
+    private String claimantBenefitsDetail;
 }
