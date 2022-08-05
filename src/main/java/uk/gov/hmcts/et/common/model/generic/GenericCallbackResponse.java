0c5725b8adadae1ade9e3ff01afab2509a690e78
hsjhita <harpreet.jhita@justice.gov.uk>
2022-08-05 15:42:37 +0100

    Update GenericCallbackResponse.java

--- src/main/java/uk/gov/hmcts/et/common/model/generic/GenericCallbackResponse.java
+++ src/main/java/uk/gov/hmcts/et/common/model/generic/GenericCallbackResponse.java
@@ -14,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @SuperBuilder
 @JsonIgnoreProperties(ignoreUnknown = true)
+@SuppressWarnings("checkstyle:MemberName")
 public class GenericCallbackResponse {
 
     private List<String> errors;
