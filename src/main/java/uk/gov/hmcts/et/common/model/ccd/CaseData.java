2ef0a413b5fd4cf0761464deecec5de272af9116
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2023-01-05 11:42:06 +0000

    Update CaseData

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/CaseData.java
@@ -1092,27 +1092,27 @@ public class CaseData extends Et1CaseData {
     private String resTseTableMarkUp;
 
     //TSE Admin Respond to an application
-    @JsonProperty("tseAdmRespondEnterResponseTitle")
-    private String tseAdmRespondEnterResponseTitle;
-    @JsonProperty("tseAdmRespondAdditionalInformation")
-    private String tseAdmRespondAdditionalInformation;
-    @JsonProperty("tseAdmRespondAddDocumentMandatory")
-    private UploadedDocumentType tseAdmRespondAddDocumentMandatory;
-    @JsonProperty("tseAdmRespondAddDocumentOptional")
-    private UploadedDocumentType tseAdmRespondAddDocumentOptional;
-    @JsonProperty("tseAdmRespondIsCmoOrRequest")
-    private String tseAdmRespondIsCmoOrRequest;
-    @JsonProperty("tseAdmRespondCmoMadeBy")
-    private String tseAdmRespondCmoMadeBy;
-    @JsonProperty("tseAdmRespondRequestMadeBy")
-    private String tseAdmRespondRequestMadeBy;
-    @JsonProperty("tseAdmRespondEnterFullName")
-    private String tseAdmRespondEnterFullName;
-    @JsonProperty("tseAdmRespondIsResponseRequired")
-    private String tseAdmRespondIsResponseRequired;
-    @JsonProperty("tseAdmRespondSelectPartyRespond")
-    private String tseAdmRespondSelectPartyRespond;
-    @JsonProperty("tseAdmRespondSelectPartyNotify")
-    private String tseAdmRespondSelectPartyNotify;
+    @JsonProperty("tseAdmReplyEnterResponseTitle")
+    private String tseAdmReplyEnterResponseTitle;
+    @JsonProperty("tseAdmReplyAdditionalInformation")
+    private String tseAdmReplyAdditionalInformation;
+    @JsonProperty("tseAdmReplyAddDocumentMandatory")
+    private UploadedDocumentType tseAdmReplyAddDocumentMandatory;
+    @JsonProperty("tseAdmReplyAddDocumentOptional")
+    private UploadedDocumentType tseAdmReplyAddDocumentOptional;
+    @JsonProperty("tseAdmReplyIsCmoOrRequest")
+    private String tseAdmReplyIsCmoOrRequest;
+    @JsonProperty("tseAdmReplyCmoMadeBy")
+    private String tseAdmReplyCmoMadeBy;
+    @JsonProperty("tseAdmReplyRequestMadeBy")
+    private String tseAdmReplyRequestMadeBy;
+    @JsonProperty("tseAdmReplyEnterFullName")
+    private String tseAdmReplyEnterFullName;
+    @JsonProperty("tseAdmReplyIsResponseRequired")
+    private String tseAdmReplyIsResponseRequired;
+    @JsonProperty("tseAdmReplySelectPartyRespond")
+    private String tseAdmReplySelectPartyRespond;
+    @JsonProperty("tseAdmReplySelectPartyNotify")
+    private String tseAdmReplySelectPartyNotify;
 
 }
