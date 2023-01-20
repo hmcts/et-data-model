ab1f3486155d09c05421eef8cb4fc1017730d646
hsjhita <harpreet.jhita@justice.gov.uk>
2023-01-20 16:21:14 +0000

    Update CaseData.java

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/CaseData.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.et.common.model.ccd;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
