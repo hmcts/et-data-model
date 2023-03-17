263038c50f5050747dafa0234423669923c2a55c
gauravchawla <gaurav-chawla@hotmail.com>
2023-03-17 11:09:26 +0000

    RET-2982

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/types/AllocateHearingType.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/types/AllocateHearingType.java
@@ -8,31 +8,16 @@ import uk.gov.hmcts.et.common.model.bulk.types.DynamicFixedListType;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 public class AllocateHearingType {
+    @JsonProperty("allocateHearingDate")
+    private String allocateHearingDate;
     @JsonProperty("allocateHearingVenue")
     private DynamicFixedListType allocateHearingVenue;
     @JsonProperty("allocateHearingRoom")
     private DynamicFixedListType allocateHearingRoom;
     @JsonProperty("allocateHearingClerk")
     private DynamicFixedListType allocateHearingClerk;
-    @JsonProperty("allocateHearingSitAlone")
-    private String allocateHearingSitAlone;
-    @JsonProperty("allocateHearingJudge")
-    private DynamicFixedListType allocateHearingJudge;
-    @JsonProperty("allocateHearingEmployerMember")
-    private DynamicFixedListType allocateHearingEmployerMember;
-    @JsonProperty("allocateHearingEmployeeMember")
-    private DynamicFixedListType allocateHearingEmployeeMember;
     @JsonProperty("allocateHearingPostponedBy")
     private String allocateHearingPostponedBy;
     @JsonProperty("allocateHearingStatus")
     private String allocateHearingStatus;
-
-    public boolean hasAllocateHearingEmployerMember() {
-        return allocateHearingEmployerMember != null && allocateHearingEmployerMember.getValue() != null;
-    }
-
-    public boolean hasAllocateHearingEmployeeMember() {
-        return allocateHearingEmployeeMember != null && allocateHearingEmployeeMember.getValue() != null;
-    }
-
 }
