2574bbd7d22499a2bbbe2f5d46c1abd6d8dd483f
gauravchawla <gaurav-chawla@hotmail.com>
2023-03-17 16:29:39 +0000

    RET-2982

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/items/HearingDetailTypeItem.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/items/HearingDetailTypeItem.java
@@ -1,8 +1,12 @@
 package uk.gov.hmcts.et.common.model.ccd.items;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 import uk.gov.hmcts.et.common.model.ccd.types.HearingDetailType;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
 public class HearingDetailTypeItem {
     @JsonProperty("id")
     private String id;
