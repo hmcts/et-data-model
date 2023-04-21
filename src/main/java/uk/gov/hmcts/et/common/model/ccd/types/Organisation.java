a909764bce1f92e021696cc7490477c03cf5ff46
Itay-Kirsh <Itayk747@gmail.com>
2023-04-21 15:32:50 +0100

    Remove address from Organisation As part of 3433 and 3146, we can't save the address on organisation and will have to override the respondent's address with the MyHMCTS address.

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/types/Organisation.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/types/Organisation.java
@@ -15,6 +15,4 @@ public class Organisation {
     private String organisationID;
     @JsonProperty("OrganisationName")
     private String organisationName;
-    @JsonProperty("OrganisationAddress")
-    private OrganisationAddress organisationAddress;
 }
