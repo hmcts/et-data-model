0c86a936a76078ccbfbafba75ca165bf96265681
Danny Furnivall <danielfurnivall@gmail.com>
2023-07-17 12:46:24 +0100

    Update CaseData.java

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/CaseData.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.et.common.model.ccd;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import uk.gov.hmcts.et.common.model.bulk.types.DynamicFixedListType;
