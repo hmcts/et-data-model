7fb2779eccea13ff5e593042123af0c3520e0e4b
marcjudge <marc.judge@hmcts.net>
2023-07-19 16:36:27 +0100

    Change datetime to string

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/types/CaseLink.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/types/CaseLink.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -15,7 +14,7 @@ public class CaseLink {
     @JsonProperty("CaseType")
     private String caseType;
     @JsonProperty("CreatedDateTime")
-    private LocalDateTime createdDateTime;
+    private String createdDateTime;
     @JsonProperty("ReasonForLink")
     private List<LinkReason> reasonForLink;
 }
