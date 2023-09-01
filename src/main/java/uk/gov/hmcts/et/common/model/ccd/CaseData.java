91b19599af50e61abf3567a25661414c4e913329
mehmet.dede <mehmet.dede@justice.gov.uk>
2023-09-01 15:00:46 +0100

    Added systemUser attribute to CaseData model

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/CaseData.java
@@ -1428,6 +1428,6 @@ public class CaseData extends Et1CaseData {
     @JsonProperty("caseLinks")
     private ListTypeItem<CaseLink> caseLinks;
 
-    @JsonProperty("isSystemUser")
+    @JsonProperty("systemUser")
     private boolean systemUser;
 }
