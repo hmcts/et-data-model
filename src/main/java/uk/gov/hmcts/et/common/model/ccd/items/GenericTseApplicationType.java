db57e6f997c92a1195283dc7893697b0cb9ea11a
aaronjduffy <Aaron.Duffy@HMCTS.NET>
2023-09-14 16:37:28 +0100

    removed TseAdminRecordDecisionTypeItem and updated relative files

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/items/GenericTseApplicationType.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/items/GenericTseApplicationType.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import uk.gov.hmcts.et.common.model.ccd.types.TseAdminRecordDecisionType;
 import uk.gov.hmcts.et.common.model.ccd.types.TseRespondType;
 import uk.gov.hmcts.et.common.model.ccd.types.UploadedDocumentType;
 
@@ -53,6 +54,6 @@ public class GenericTseApplicationType {
     private ListTypeItem<TseRespondType> respondCollection;
 
     @JsonProperty("adminDecision")
-    private List<TseAdminRecordDecisionTypeItem> adminDecision;
+    private ListTypeItem<TseAdminRecordDecisionType> adminDecision;
 
 }
