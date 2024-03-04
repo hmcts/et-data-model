169c1ecd8d90f6381d66ea29b1e19e838b416986
btensay <tensay.bulcha@gmail.com>
2024-03-04 20:41:51 +0000

    new field added

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/CaseData.java
@@ -141,6 +141,8 @@ public class CaseData extends Et1CaseData {
     private String userLocation;
     @JsonProperty("documentCollection")
     private List<DocumentTypeItem> documentCollection;
+    @JsonProperty("uploadDocumentCollection")
+    private List<DocumentTypeItem> uploadDocumentCollection;
     @JsonProperty("claimantDocumentCollection")
     private List<DocumentTypeItem> claimantDocumentCollection;
     @JsonProperty("correspondenceScotType")
