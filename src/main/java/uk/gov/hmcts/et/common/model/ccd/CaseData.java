c499304050eb733ff04952b612a1933e8c821e61
mehmet.dede <mehmet.dede@justice.gov.uk>
2024-04-03 15:37:44 +0100

    Removed respondentFlags

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/CaseData.java
@@ -1372,7 +1372,6 @@ public class CaseData extends Et1CaseData {
 
     // Case Flags
     private CaseFlagsType caseFlags;
-    private CaseFlagsType respondentFlags;
     private CaseFlagsType claimantInternalFlags;
     private CaseFlagsType claimantExternalFlags;
     private CaseFlagsType respondentInternalFlags;
