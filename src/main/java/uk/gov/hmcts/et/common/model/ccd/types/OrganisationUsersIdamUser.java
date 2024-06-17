a217848c4bf09b815c06eed1eb26b8a78797b624
Jack Reeve <jack@reevies.tech>
2024-06-17 15:44:25 +0100

    added Jackonized

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/types/OrganisationUsersIdamUser.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/types/OrganisationUsersIdamUser.java
@@ -1,12 +1,15 @@
 package uk.gov.hmcts.et.common.model.ccd.types;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Builder;
 import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 
 @Builder
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Jacksonized
 public class OrganisationUsersIdamUser {
     private String userIdentifier;
     private String firstName;
--- src/main/java/uk/gov/hmcts/et/common/model/ccd/types/OrganisationUsersResponse.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/types/OrganisationUsersResponse.java
@@ -1,13 +1,17 @@
 package uk.gov.hmcts.et.common.model.ccd.types;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Builder;
 import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
 import java.util.List;
 
 @Builder
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Jacksonized
 public class OrganisationUsersResponse {
     private String organisationIdentifier;
     private List<OrganisationUsersIdamUser> users;
