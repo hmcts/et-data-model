df8f315cae47cee73ff087adb8229519d01109a6
Jack Reeve <jack@reevies.tech>
2024-06-19 13:02:13 +0100

    checkstyle

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/types/OrganisationUsersIdamUser.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/types/OrganisationUsersIdamUser.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.et.common.model.ccd.types;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
