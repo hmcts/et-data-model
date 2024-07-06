5263c34ae2ac34510bac26f585ad4ae9fc013192
Lahiru Oshara Hinguruduwa <35135059+LahiruOshara@users.noreply.github.com>
2024-07-06 11:14:43 +0100

    remove field claimantTseSuppliedDocuments

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/CaseData.java
@@ -1143,9 +1143,6 @@ public class CaseData extends Et1CaseData {
     private String claimantTseTextBox13;
     @JsonProperty("claimantTseRule92AnsNoGiveDetails")
     private String claimantTseRule92AnsNoGiveDetails;
-    @JsonProperty("claimantTseSuppliedDocuments")
-    private String claimantTseSuppliedDocuments;
-
 
     //TSE Admin Record a Decision
     @JsonProperty("tseAdminSelectApplication")
