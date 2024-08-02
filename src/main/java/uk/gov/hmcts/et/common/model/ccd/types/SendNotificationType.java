8c9e38bb4046e27c02f727d5e25b67564062314b
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2024-08-02 18:49:21 +0100

    Update SendNotificationType.java

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/types/SendNotificationType.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/types/SendNotificationType.java
@@ -67,8 +67,8 @@ public class SendNotificationType {
     private List<GenericTypeItem<RespondNotificationType>> respondNotificationTypeCollection;
     @JsonProperty("notificationState")
     private String notificationState;
-    @JsonProperty("notificationHearingState")
-    private String notificationHearingState;
+    @JsonProperty("hearingClaimantViewState")
+    private String hearingClaimantViewState;
     @JsonProperty("sendNotificationSubjectString")
     private String sendNotificationSubjectString;
     @JsonProperty("sendNotificationResponseTribunalTable")
