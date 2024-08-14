ab9b64f1b40d15dce8cd30f1d307bb0559074146
mehmet.dede <mehmet.dede@justice.gov.uk>
2024-08-14 14:13:29 +0100

    Update to test of CaseAccessPinTest to use directly LocalDateTime.now().toString method.

--- src/test/java/uk/gov/hmcts/et/common/model/ccd/CaseAccessPinTest.java
+++ src/test/java/uk/gov/hmcts/et/common/model/ccd/CaseAccessPinTest.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,29 +13,26 @@ public class CaseAccessPinTest {
 
     private static final int EXPIRY_PERIOD = 180;
     private static final int CASE_ACCESS_PIN_LENGTH = 12;
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+    private static final String DATE_NOW = LocalDateTime.now().plusDays(EXPIRY_PERIOD).toString();
 
     @Test
     public void testDeserializeCaseAccessPin() throws IOException {
-        String dateNow = LocalDateTime.now().plusDays(EXPIRY_PERIOD).format(DATE_TIME_FORMATTER);
-        String json = "{\"expiryDate\":\"" + dateNow + "\",\"caseAccessibleRoles\":[\"[DEFENDANT]\"]}";
+        String json = "{\"expiryDate\":\"" + DATE_NOW + "\",\"caseAccessibleRoles\":[\"[DEFENDANT]\"]}";
         CaseAccessPin caseAccessPin = new ObjectMapper().readValue(json, CaseAccessPin.class);
 
         assertThat(caseAccessPin.getAccessCode()).isNotBlank();
-        assertThat(caseAccessPin.getExpiryDate()).isEqualTo(dateNow);
+        assertThat(caseAccessPin.getExpiryDate()).isEqualTo(DATE_NOW);
         assertThat(caseAccessPin.getCaseAccessibleRoles()).contains("[DEFENDANT]");
     }
 
     @Test
     public void testCaseAccessPinBuilder() {
-
-        String dateNow = LocalDate.now().plusDays(EXPIRY_PERIOD).format(DATE_TIME_FORMATTER);
         CaseAccessPin caseAccessPin = new CaseAccessPin.Builder()
-                .expiryDate(dateNow)
+                .expiryDate(DATE_NOW)
                 .caseAccessibleRoles(List.of("[DEFENDANT]", "[CITIZEN]"))
                 .build();
         assertThat(caseAccessPin.getAccessCode()).hasSize(CASE_ACCESS_PIN_LENGTH);
-        assertThat(caseAccessPin.getExpiryDate()).isEqualTo(dateNow);
+        assertThat(caseAccessPin.getExpiryDate()).isEqualTo(DATE_NOW);
         assertThat(caseAccessPin.getCaseAccessibleRoles()).contains("[DEFENDANT]");
     }
 
