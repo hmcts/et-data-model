99d7968054ba6af1ab32de12aa91f41543fe92e9
btensay <tensay.bulcha@gmail.com>
2024-10-29 14:48:25 +0000

    token change to fix lib tag issue

--- src/main/java/uk/gov/hmcts/et/common/model/ccd/types/RespondentSumType.java
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/types/RespondentSumType.java
@@ -41,10 +41,12 @@ public class RespondentSumType {
     private String responseReturnedFromJudge;
     @JsonProperty("respondent_name")
     private String respondentName;
+
     @JsonProperty("respondent_hearing_panel_preference")
     private String respondentHearingPanelPreference;
     @JsonProperty("respondent_hearing_panel_preference_reason")
     private String respondentHearingPanelPreferenceReason;
+
     @JsonProperty("respondentType")
     private String respondentType;
     @JsonProperty("respondentOrganisation")
