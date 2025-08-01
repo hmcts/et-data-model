8319be096f05f94021dd62870233ab7f03e14870
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2025-08-01 08:31:34 +0100

    Remove unused import statement from DynamicFixedListType.java

--- src/main/java/uk/gov/hmcts/et/common/model/bulk/types/DynamicFixedListType.java
+++ src/main/java/uk/gov/hmcts/et/common/model/bulk/types/DynamicFixedListType.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 
