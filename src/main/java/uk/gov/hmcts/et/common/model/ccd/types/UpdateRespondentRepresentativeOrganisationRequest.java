ab06f34757a018c9feae8562c58eb8758be0fbcd
Mehmet Tahir Dede <mehmet.dede@justice.gov.uk>
2025-09-19 15:14:03 +0100

    Added new model UpdateRespondentRepresentativeOrganisationRequest

---
+++ src/main/java/uk/gov/hmcts/et/common/model/ccd/types/UpdateRespondentRepresentativeOrganisationRequest.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.et.common.model.ccd.types;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
+@Data
+@Builder
+@Jacksonized
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UpdateRespondentRepresentativeOrganisationRequest {
+    @JsonProperty("ChangeOrganisationRequest")
+    private ChangeOrganisationRequest changeOrganisationRequest;
+    @JsonProperty("RespondentName")
+    private String respondentName;
+}
---
+++ src/test/java/uk/gov/hmcts/et/common/model/ccd/types/UpdateRespondentRepresentativeOrganisationRequestTestConstants.java
@@ -0,0 +1,11 @@
+package uk.gov.hmcts.et.common.model.ccd.types;
+
+public final class UpdateRespondentRepresentativeOrganisationRequestTestConstants {
+    public static final String ORGANISATION_TO_ADD_ORGANISATION_ID = "Test Organisation to Add ID";
+    public static final String ORGANISATION_TO_ADD_ORGANISATION_NAME = "Test Organisation to Add Name";
+    public static final String ORGANISATION_TO_REMOVE_ORGANISATION_ID = "Test Organisation to Remove ID";
+    public static final String ORGANISATION_TO_REMOVE_ORGANISATION_NAME = "Test Organisation to Remove Name";
+    public static final String CASE_ROLE_ID = "Test Case Role ID";
+    public static final String APPROVAL_STATUS = "APPROVED";
+    public static final String RESPONDENT_NAME = "Test Respondent";
+}
